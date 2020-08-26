dcd08f40404070d70e758f07356193c28b1d5f39
yahia <yhegazy@users.noreply.github.com>
2020-08-26 10:37:34 -0400

    Fifth week of updated MOOC.fi Java Course

---
+++ part05-Part05_10.CardPayments/src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
+
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatAffordably(5);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
+
+        System.out.println(unicafeExactum);
+    }
+}
