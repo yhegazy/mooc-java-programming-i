84e3d048e178f9f72c819f476f72fed715916cc5
yahia <yhegazy@users.noreply.github.com>
2020-09-02 12:18:05 -0400

    Sixth week of updated MOOC.fi Java Course

---
+++ part06-Part06_05.SantasWorkshop/src/main/java/Package.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Package {
+
+    private ArrayList<Gift> gifts;
+
+    public Package(){
+        this.gifts = new ArrayList<>();
+    }
+
+    public void addGift(Gift gift) {
+        this.gifts.add(gift);
+    }
+
+    public int totalWeight(){
+        int totalWeight = 0;
+
+        for (Gift item: gifts){
+            totalWeight += item.getWeight();
+        }
+
+        return totalWeight;
+    }
+}
+
+
